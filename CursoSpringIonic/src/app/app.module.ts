0fd019b279b68d6f50544bfa2024e7acb25ece59
DevTef <thominhaserick@gmail.com>
2020-04-15 22:55:18 -0300

    Lazy Load

--- CursoSpringIonic/src/app/app.module.ts
+++ CursoSpringIonic/src/app/app.module.ts
@@ -3,16 +3,15 @@ import { ErrorHandler, NgModule } from "@angular/core";
 import { IonicApp, IonicErrorHandler, IonicModule } from "ionic-angular";
 
 import { MyApp } from "./app.component";
-import { HomePage } from "../pages/home/home";
 
 import { StatusBar } from "@ionic-native/status-bar";
 import { SplashScreen } from "@ionic-native/splash-screen";
 
 @NgModule({
-  declarations: [MyApp, HomePage],
+  declarations: [MyApp],
   imports: [BrowserModule, IonicModule.forRoot(MyApp)],
   bootstrap: [IonicApp],
-  entryComponents: [MyApp, HomePage],
+  entryComponents: [MyApp],
   providers: [
     StatusBar,
     SplashScreen,
