71dd5e0500dd15e0d4c9c8975017ae81cdc430e4
DevTef <thominhaserick@gmail.com>
2020-04-21 13:58:53 -0300

    interceptor for token a requisition

--- CursoSpringIonic/src/services/domain/cliente.service.ts
+++ CursoSpringIonic/src/services/domain/cliente.service.ts
@@ -11,12 +11,8 @@ export class ClienteService {
         public storage: StorageService) { }
 
     findByEmail(email: string): Observable<ClienteDTO> {
-        let token = this.storage.getLocalUser().token;
-        let authHeader = new HttpHeaders({ 'Authorization': 'Bearer ' + token });
-
         return this.http.get<ClienteDTO>
-            (`${API_CONFIG.baseUrl}clientes/email?value=${email}`,
-                { 'headers': authHeader });
+            (`${API_CONFIG.baseUrl}clientes/email?value=${email}`);
     }
 
     getImageFromBucket(id: string): Observable<any> {
