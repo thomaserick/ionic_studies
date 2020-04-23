3eced8081afbe1db98811ab867b67e7acab2740c
DevTef <thominhaserick@gmail.com>
2020-04-22 21:55:11 -0300

    Loading image of products

--- CursoSpringIonic/src/pages/produtos/produtos.ts
+++ CursoSpringIonic/src/pages/produtos/produtos.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
 import { ProdutoDTO } from '../../app/models/produto.dto';
 import { ProdutoService } from '../../services/domain/produto.service';
+import { API_CONFIG } from '../../config/api.config';
 
 
 @IonicPage()
@@ -23,7 +24,22 @@ export class ProdutosPage {
     this.produtoService.findByCategoria(categoria_id)
       .subscribe(response => {
         this.produtos = response['content'];
+        this.loadImageUrls();
       }, error => { })
 
   }
+
+  loadImageUrls() {
+    for (var i = 0; i < this.produtos.length; i++) {
+      let produto = this.produtos[i];
+
+      this.produtoService.getSmallImgFromBucket(produto.id)
+        .subscribe(response => {
+          produto.imageUrl = `${API_CONFIG.bucketBaseUrl}prod${produto.id}-small.jpg`;
+        }, error => { })
+    }
+  }
+
+
+
 }
--- CursoSpringIonic/src/services/domain/produto.service.ts
+++ CursoSpringIonic/src/services/domain/produto.service.ts
@@ -1,6 +1,7 @@
 import { HttpClient } from "@angular/common/http";
 import { Injectable } from "@angular/core";
 import { API_CONFIG } from "../../config/api.config";
+import { Observable } from "rxjs/Rx";
 
 @Injectable()
 export class ProdutoService {
@@ -10,4 +11,12 @@ export class ProdutoService {
     findByCategoria(categoria_id: string) {
         return this.http.get(`${API_CONFIG.baseUrl}produtos/?categorias=${categoria_id}`);
     }
+
+    getSmallImgFromBucket(id: string): Observable<any> {
+        let url = `${API_CONFIG.bucketBaseUrl}prod${id}-small.jpg`;
+        return this.http.get(url, {
+            responseType: 'blob'
+        });
+    }
+
 }
