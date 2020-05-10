86d50a0d6477092342b3c02d15485c0469c4bf06
Thomas Erick <thominhaserick@gmail.com>
2020-05-10 12:22:06 -0300

    Loading component

--- CursoSpringIonic/src/pages/produtos/produtos.ts
+++ CursoSpringIonic/src/pages/produtos/produtos.ts
@@ -1,48 +1,68 @@
-import { Component } from '@angular/core';
-import { IonicPage, NavController, NavParams } from 'ionic-angular';
-import { ProdutoDTO } from '../../app/models/produto.dto';
-import { ProdutoService } from '../../services/domain/produto.service';
-import { API_CONFIG } from '../../config/api.config';
-
+import { Component } from "@angular/core";
+import {
+  IonicPage,
+  NavController,
+  NavParams,
+  LoadingController,
+} from "ionic-angular";
+import { ProdutoDTO } from "../../app/models/produto.dto";
+import { ProdutoService } from "../../services/domain/produto.service";
+import { API_CONFIG } from "../../config/api.config";
 
 @IonicPage()
 @Component({
-  selector: 'page-produtos',
-  templateUrl: 'produtos.html',
+  selector: "page-produtos",
+  templateUrl: "produtos.html",
 })
 export class ProdutosPage {
-
   produtos: ProdutoDTO[];
 
-  constructor(public navCtrl: NavController,
+  constructor(
+    public navCtrl: NavController,
     public navParams: NavParams,
-    public produtoService: ProdutoService) {
-  }
+    public produtoService: ProdutoService,
+    public loadCtrl: LoadingController
+  ) {}
 
   ionViewDidLoad() {
-    let categoria_id = this.navParams.get('categoria_id');
-    this.produtoService.findByCategoria(categoria_id)
-      .subscribe(response => {
-        this.produtos = response['content'];
-        this.loadImageUrls();
-      }, error => { })
+    let categoria_id = this.navParams.get("categoria_id");
 
+    //Loading
+    let loader = this.presentLoading();
+
+    this.produtoService.findByCategoria(categoria_id).subscribe(
+      (response) => {
+        this.produtos = response["content"];
+        loader.dismiss();
+        this.loadImageUrls();
+      },
+      (error) => {
+        loader.dismiss();
+      }
+    );
   }
 
   loadImageUrls() {
     for (var i = 0; i < this.produtos.length; i++) {
       let produto = this.produtos[i];
-      this.produtoService.getSmallImgFromBucket(produto.id)
-        .subscribe(response => {
+      this.produtoService.getSmallImgFromBucket(produto.id).subscribe(
+        (response) => {
           produto.imageUrl = `${API_CONFIG.bucketBaseUrl}prod${produto.id}-small.jpg`;
-        }, error => { })
+        },
+        (error) => {}
+      );
     }
   }
 
   showDetail(produto_id: string) {
-    this.navCtrl.push('ProdutoDetailPage', { produto_id: produto_id });
+    this.navCtrl.push("ProdutoDetailPage", { produto_id: produto_id });
   }
 
-
-
+  presentLoading() {
+    let loader = this.loadCtrl.create({
+      content: "Aguarde...",
+    });
+    loader.present();
+    return loader;
+  }
 }
